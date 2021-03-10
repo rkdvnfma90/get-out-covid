5dcf6fcefba503dea1207617f2a5d56da972fdea
DESKTOP-BGEFQLI\Administrator <rkdvnfma90@hanmail.net>
2021-03-10 22:16:21 +0900

    refactor 주석 제거

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -33,55 +33,6 @@ const Cards = ({data : {confirmed, deaths, recovered, lastUpdate}}) => {
           lastUpdate={lastUpdate}
           cardSubtitle="코로나로 인한 사망자"
         />
-
-        {/* <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
-          <CardContent>
-            <Typography color="textSecondary" gutterBottom>
-              감염
-            </Typography>
-            <Typography variant="h5">
-              <CountUp start={0} end={confirmed.value} duration={2} separator=","/>
-            </Typography>
-            <Typography color="textSecondary">
-              {new Date(lastUpdate).toLocaleDateString()}
-            </Typography>
-            <Typography variant="body2">
-              확진자
-            </Typography>
-          </CardContent>
-        </Grid>
-        <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
-          <CardContent>
-            <Typography color="textSecondary" gutterBottom>
-              완치
-            </Typography>
-            <Typography variant="h5">
-              <CountUp start={0} end={recovered.value} duration={2} separator=","/>
-            </Typography>
-            <Typography color="textSecondary">
-              Real Date
-            </Typography>
-            <Typography variant="body2">
-              완치자
-            </Typography>
-          </CardContent>
-        </Grid>
-        <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
-          <CardContent>
-            <Typography color="textSecondary" gutterBottom>
-              사망
-            </Typography>
-            <Typography variant="h5">
-              <CountUp start={0} end={deaths.value} duration={2} separator=","/>
-            </Typography>
-            <Typography color="textSecondary">
-              Real Date
-            </Typography>
-            <Typography variant="body2">
-              사망자
-            </Typography>
-          </CardContent>
-        </Grid> */}
       </Grid>
     </div>
   )
