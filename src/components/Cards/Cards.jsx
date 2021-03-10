af4f84363b7cbaef4423984539553b4f4c992a82
DESKTOP-BGEFQLI\Administrator <rkdvnfma90@hanmail.net>
2021-03-10 17:09:02 +0900

    refactor 레이아웃 수정

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,19 +1,40 @@
 import React from 'react'
-import {Card, CardContent, Typography, Grid} from '@material-ui/core'
+import { Grid } from '@material-ui/core'
 import styles from './Cards.module.css'
-import CountUp from 'react-countup'
-import cx from 'classnames'
+import CardComponent from './Card/Card'
 
 const Cards = ({data : {confirmed, deaths, recovered, lastUpdate}}) => {
 
-  if(!confirmed || !deaths || !recovered || !lastUpdate) {
+  if(!confirmed) {
     return '로딩중...'
   }
   
   return (
     <div className={styles.container}>
-      <Grid container spacing={3} justify="center">
-        <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
+      <Grid container spacing={10} justify="center">
+        <CardComponent
+          className={styles.infected}
+          cardTitle="감염"
+          value={confirmed.value}
+          lastUpdate={lastUpdate}
+          cardSubtitle="코로나 확진자"
+        />
+        <CardComponent
+          className={styles.recovered}
+          cardTitle="완치"
+          value={recovered.value}
+          lastUpdate={lastUpdate}
+          cardSubtitle="코로나 완치자"
+        />
+        <CardComponent
+          className={styles.deaths}
+          cardTitle="사망"
+          value={deaths.value}
+          lastUpdate={lastUpdate}
+          cardSubtitle="코로나로 인한 사망자"
+        />
+
+        {/* <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
           <CardContent>
             <Typography color="textSecondary" gutterBottom>
               감염
@@ -60,7 +81,7 @@ const Cards = ({data : {confirmed, deaths, recovered, lastUpdate}}) => {
               사망자
             </Typography>
           </CardContent>
-        </Grid>
+        </Grid> */}
       </Grid>
     </div>
   )
