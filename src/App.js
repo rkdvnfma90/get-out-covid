3828b919ac6e407aa95ce6f133a1a4d282f03ef8
DESKTOP-BGEFQLI\Administrator <rkdvnfma90@hanmail.net>
2021-03-10 14:07:24 +0900

    feat 감염자, 완치자, 사망자 표시

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Cards, Chart, CountryPicker } from './components'
 import styles from './App.module.css'
+import { fetchData } from './api'
 
 const App = () => {
+  const [data, setData] = useState({})
+
+  useEffect(() => {
+    const fetchCovidData = async () => {
+      const fetchedData = await fetchData()
+      console.log(fetchedData)
+      setData(fetchedData)
+    }
+
+    fetchCovidData()
+  }, [])
+
   return (
     <div className={styles.container}>
-      <Cards />
+      <Cards data={data} />
       <CountryPicker />
       <Chart />
     </div>
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,67 @@
 import React from 'react'
+import {Card, CardContent, Typography, Grid} from '@material-ui/core'
+import styles from './Cards.module.css'
+import CountUp from 'react-countup'
+import cx from 'classnames'
 
-const Cards = () => {
+const Cards = ({data : {confirmed, deaths, recovered, lastUpdate}}) => {
+
+  if(!confirmed || !deaths || !recovered || !lastUpdate) {
+    return '로딩중...'
+  }
+  
   return (
-    <div>
-      <h1>Cards</h1>
+    <div className={styles.container}>
+      <Grid container spacing={3} justify="center">
+        <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              감염
+            </Typography>
+            <Typography variant="h5">
+              <CountUp start={0} end={confirmed.value} duration={2} separator=","/>
+            </Typography>
+            <Typography color="textSecondary">
+              {new Date(lastUpdate).toLocaleDateString()}
+            </Typography>
+            <Typography variant="body2">
+              확진자
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              완치
+            </Typography>
+            <Typography variant="h5">
+              <CountUp start={0} end={recovered.value} duration={2} separator=","/>
+            </Typography>
+            <Typography color="textSecondary">
+              Real Date
+            </Typography>
+            <Typography variant="body2">
+              완치자
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              사망
+            </Typography>
+            <Typography variant="h5">
+              <CountUp start={0} end={deaths.value} duration={2} separator=","/>
+            </Typography>
+            <Typography color="textSecondary">
+              Real Date
+            </Typography>
+            <Typography variant="body2">
+              사망자
+            </Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
     </div>
   )
 }
